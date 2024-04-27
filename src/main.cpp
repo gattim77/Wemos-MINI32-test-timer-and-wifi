2b36f98045598b70d6a2a7b4b7626608f63d592a
Marco Gatti <gattim@gmail.com>
2024-04-27 13:25:07 +0200

    cambi vari

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
-
-#define WIFI_SSID ${{WIFI_SSID}}
+//int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
+  Serial.begin(74880);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+Serial.println(SSID_NAME);
+Serial.println(SSID_PASSWORD);
+
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+//int myFunction(int x, int y) {
+//  return x + y;
+//}
